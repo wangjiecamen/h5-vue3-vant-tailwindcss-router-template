de7cf5515134ef27da6b3ea1777d781080edadad
王 杰 <wangjie1@chinagoods.com>
2023-11-20 08:39:44 +0800

    eslint

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,14 @@
-<template>
-  <van-nav-bar title="moby"></van-nav-bar>
-<div class="text-[20px]">home</div>
-<van-button>moby</van-button>
-
-</template>
 <script lang="ts" setup>
 
 </script>
+
+<template>
+  <van-nav-bar title="moby" />
+  <div class="text-[20px]">
+    home
+  </div>
+  <van-button>moby</van-button>
+</template>
 
 <style scoped lang="scss">
 </style>
